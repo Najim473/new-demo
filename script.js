78424d9f132fad6b98867687ff797d95487d5a38
Najim Uddin <najim7304@gmail.com>
2021-11-21 19:26:19 +0600

    added all script

---
+++ script.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title>repl.it</title>
+	<link href="CSS/style.css" rel="stylesheet" type="text/css" />
+	<link href="CSS/style.scss" rel="stylesheet" type="text/css">
+	<link href="CSS/all.css" rel="stylesheet" type="text/css">
+	<link href="CSS/style.map.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<div class="container">
+		<div class="profile">
+			<img src="img/Joe Biden.jpg" alt="profile">
+               </div>
+               <div class="about">
+                       <div class="online">
+                <a class="instagram"href="#"><i class="fab fa-instagram"></i></a>
+               <a class="facebook" href="#"><i class="fab fa-facebook"></i></a>
+               <a class="Email"href="#"><i class="fas fa-envelope"></i></a>
+               <a class="twitter" href="#"><i class="fab fa-twitter-square"></i></a>
+                       </div>
+                       <div class="details">
+                               <p class="name">President Joe Biden</p>
+                               <p class="title">President of the USA</p>
+                               <p class="about"> Joseph Robinette Biden Jr. is an American politician and president-elect of the United States. After defeating incumbent Donald Trump in the 2020 United States<br> presidential election, he will be inaugurated as the 46th president in January 2021</p>
+
+                       </div>
+               </div>
+       </div
+       <hr>
+ </body>
+ <script src="script.js"></script>
+</html>
